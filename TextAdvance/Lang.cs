4fc413a17add276d70b845d985ddaf175cc7f790
Inseok Lee <git@dlun.ch>
2024-12-23 15:09:30 +0900

    fix

--- TextAdvance/Lang.cs
+++ TextAdvance/Lang.cs
@@ -2,22 +2,22 @@
 
 internal static class Lang
 {
-    internal static string[] AcceptStr = ["Accept", "接受", "Annehmen", "Accepter", "受注"];
-    internal static string[] SkipCutsceneStr = ["Skip cutscene?", "要跳过这段过场动画吗？", "要跳過這段過場動畫嗎？", "Videosequenz überspringen?", "Passer la scène cinématique ?", "このカットシーンをスキップしますか？"];
-    internal static string[] YesStr = ["Yes.", "是", "Ja", "Oui", "はい"];
-    internal static string[] CompleteStr = ["Complete", "完成", "Abschließen", "Accepter", "コンプリート"];
+    internal static string[] AcceptStr = ["Accept", "接受", "Annehmen", "Accepter", "受注", "수락"];
+    internal static string[] SkipCutsceneStr = ["Skip cutscene?", "要跳过这段过场动画吗？", "要跳過這段過場動畫嗎？", "Videosequenz überspringen?", "Passer la scène cinématique ?", "このカットシーンをスキップしますか？", "영상을 건너뛰시겠습니까?"];
+    internal static string[] YesStr = ["Yes.", "是", "Ja", "Oui", "はい", "예"];
+    internal static string[] CompleteStr = ["Complete", "完成", "Abschließen", "Accepter", "コンプリート", "완료"];
 
     /*Scaevan accessories of fending coffer
     Scaevan accessories of slaying coffer
     Scaevan accessories of aiming coffer
     Scaevan accessories of healing coffer
     Scaevan accessories of casting coffer*/
-    internal static string[] CofferOfAiming = ["aiming"];
-    internal static string[] CofferOfStriking = ["striking"];
-    internal static string[] CofferOfHealing = ["healing"];
-    internal static string[] CofferOfSlaying = ["slaying"];
-    internal static string[] CofferOfCasting = ["casting"];
-    internal static string[] CofferOfFending = ["fending"];
-    internal static string[] CofferOfMaiming = ["maiming"];
-    internal static string[] CofferOfScouting = ["scouting"];
+    internal static string[] CofferOfAiming = ["aiming", "유격대"];
+    internal static string[] CofferOfStriking = ["striking", "타격대"];
+    internal static string[] CofferOfHealing = ["healing", "치유사"];
+    internal static string[] CofferOfSlaying = ["slaying", "공격대"];
+    internal static string[] CofferOfCasting = ["casting", "마술사"];
+    internal static string[] CofferOfFending = ["fending", "수호자"];
+    internal static string[] CofferOfMaiming = ["maiming", "유격대"];
+    internal static string[] CofferOfScouting = ["scouting", "정찰대"];
 }
